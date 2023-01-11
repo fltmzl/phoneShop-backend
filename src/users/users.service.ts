96f804ed6a5f582b950d64a7f9258d9d05241500
fltmzl <fltmzl2810@gmail.com>
2023-01-11 07:16:55 +0700

    using prisma ORM

---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAll() {
+    return await this.prisma.users.findMany();
+  }
+}
