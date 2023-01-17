0cf75e2ecd06c9445edec1935c1a3b4351701d4d
fltmzl <fltmzl2810@gmail.com>
2023-01-17 10:33:24 +0700

    configure CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "credentials": true
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3300);
 }
